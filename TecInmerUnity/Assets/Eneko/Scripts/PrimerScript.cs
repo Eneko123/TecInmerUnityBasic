437321109861bd8ccacc5e341b9b01fb6f893b87
Eneko123 <eneko_iglesias@euneiz.com>
2026-02-18 17:26:20 +0100

    ns
    ns

---
+++ TecInmerUnity/Assets/Eneko/Scripts/PrimerScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PrimerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Soy el puto amo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

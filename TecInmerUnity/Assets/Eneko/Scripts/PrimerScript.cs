833a1ebc552c7a0a45f8a9fb272dbbb33d7c1162
Eneko123 <eneko_iglesias@euneiz.com>
2026-02-18 20:20:21 +0100

    Cosas que hemos hecho en clase
    "Modelar" el aula y probar cosas básicas

--- TecInmerUnity/Assets/Eneko/Scripts/PrimerScript.cs
+++ TecInmerUnity/Assets/Eneko/Scripts/PrimerScript.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class PrimerScript : MonoBehaviour
 {
+    public GameObject objeto;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Soy el puto amo");
+        objeto.transform.position = new Vector3(1, 1, 1);
     }
 
     // Update is called once per frame
